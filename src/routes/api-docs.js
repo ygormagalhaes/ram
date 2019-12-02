76bd121977f1579271f2cab7ade7a58afe1b224b
Ygor Magalhães <emaildoygor@gmail.com>
2019-12-02 16:49:55 +0000

    WIP

--- src/routes/api-docs.js
+++ src/routes/api-docs.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 const swaggerUi = require('swagger-ui-express');
 
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('../swagger.yaml');
+
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup('../swagger.json'));
+router.get('/', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
