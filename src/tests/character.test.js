8fe6fee14f7ae21a199d47555cddb46247f9822e
Ygor Magalhães <emaildoygor@gmail.com>
2019-12-02 18:29:28 +0000

    WIP

--- src/tests/character.test.js
+++ src/tests/character.test.js
@@ -1,20 +1,21 @@
 const CharacterBusiness = require('../business/character.business');
 let ApiService = require('../services/api.service');
 
-it('não deve lançar nenhuma exceção', async done => {
-    getLocations = jest.fn().mockReturnValue([]);
-    getCharacters = jest.fn().mockReturnValue([]);
-    await expect(() => {
-        CharacterBusiness.getStructuredData();
-    }).not.toThrow();
-    done();
-});
-
-xit('deve retornar um array', async done => {
-    ApiService.getLocations = jest.fn().mockResolvedValue([]);
-    ApiService.getCharacters = jest.fn().mockResolvedValue([]);
-    const result = await CharacterBusiness.getStructuredData();
-    console.log(result);
-    expect(Array.isArray(result)).toBe(true);
-    done();
-});
+describe('CharacterBusiness', () => {
+    beforeEach(() => {
+        CharacterBusiness.getLocations = jest.fn().mockResolvedValue([]);
+        CharacterBusiness.getCharacters = jest.fn().mockResolvedValue([]);
+    });
+    
+    it('não deve lançar nenhuma exceção', async done => {
+        await expect(() => {
+            CharacterBusiness.getStructuredData();
+        }).not.toThrow();
+        done();
+    });
+    
+    xit('deve retornar um array', async done => {
+        expect(Array.isArray(result)).toBe(true);
+        done();
+    });
+})
